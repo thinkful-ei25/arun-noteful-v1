5e993767bb96b87fc24e91cd57622b75e93ba475
Arun Seehra <seehra.a@gmail.com>
2018-10-08 14:08:36 -0500

    Retrieve notes based on id

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 'use strict';
 
+const express = require('express');
+
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  res.json(data.find(note => note.id === parseInt(req.params.id, 10)));
+});
+
+app
+  .listen(8080, function listen() {
+    // eslint-disable-next-line no-console
+    console.info(`Server listening on ${this.address().port}`);
+  })
+  // eslint-disable-next-line no-console
+  .on('error', err => console.error(err));
