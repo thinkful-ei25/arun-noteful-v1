a02bd7d5e86df37533cfa539b28b616eef08a70e
Arun Seehra <seehra.a@gmail.com>
2018-10-11 11:43:51 -0500

    Update client to use promises instead of callbacks

--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -44,10 +44,12 @@ const noteful = (function () {
 
       const noteId = getNoteIdFromElement(event.currentTarget);
 
-      api.details(noteId, detailsResponse => {
-        store.currentNote = detailsResponse;
-        render();
-      });
+      api
+        .details(noteId)
+        .then((detailsResponse) => {
+          store.currentNote = detailsResponse;
+          render();
+        });
 
     });
   }
@@ -59,10 +61,12 @@ const noteful = (function () {
       const searchTerm = $('.js-note-search-entry').val();
       store.currentSearchTerm = searchTerm ? { searchTerm } : {};
 
-      api.search(store.currentSearchTerm, searchResponse => {
-        store.notes = searchResponse;
-        render();
-      });
+      api
+        .search(store.currentSearchTerm)
+        .then((searchResponse) => {
+          store.notes = searchResponse;
+          render();
+        });
 
     });
   }
@@ -80,28 +84,27 @@ const noteful = (function () {
       };
 
       if (noteObj.id) {
-
-        api.update(store.currentNote.id, noteObj, updateResponse => {
-          store.currentNote = updateResponse;
-
-          api.search(store.currentSearchTerm, searchResponse => {
+        api
+          .update(store.currentNote.id, noteObj)
+          .then((updateResponse) => {
+            store.currentNote = updateResponse;
+            return api.search(store.currentSearchTerm);
+          })
+          .then((searchResponse) => {
             store.notes = searchResponse;
             render();
           });
-
-        });
-
       } else {
-
-        api.create(noteObj, createResponse => {
-          store.currentNote = createResponse;
-
-          api.search(store.currentSearchTerm, searchResponse => {
+        api
+          .create(noteObj)
+          .then((createResponse) => {
+            store.currentNote = createResponse;
+            return api.search(store.currentSearchTerm)
+          })
+          .then((searchResponse) => {
             store.notes = searchResponse;
             render();
-          });
-
-        });
+          })
       }
 
     });
