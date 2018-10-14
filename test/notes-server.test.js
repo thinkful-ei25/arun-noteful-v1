7ada2a04342b0624d07c7d52c80e3b8929275e3f
Arun Seehra <seehra.a@gmail.com>
2018-10-14 14:36:02 -0500

    Add tests for POST /api/notes

--- test/notes-server.test.js
+++ test/notes-server.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { URL } = require('url');
+
 const chai = require('chai');
 
 const app = require('../server');
@@ -118,6 +120,72 @@ describe('GET /api/:id', function () {
         .then(testFor404)
         .then(() => chai.request(app).get('/api/notes/2000'))
         .then(testFor404);
+    });
+  });
+});
+
+describe('POST /api/notes', function () {
+  context('with valid data', function () {
+    beforeEach(function () {
+      this.noteFixture = {
+        id: 1010,
+        title: 'Rabbits > Cats',
+        content: 'Everyone knows this',
+      };
+      this.agent = chai.request(app).keepOpen();
+    });
+
+    afterEach(function () {
+      this.agent.close();
+    });
+
+    it('should create and return a new item', function () {
+      return this.agent
+        .post('/api/notes')
+        .send(this.noteFixture)
+        .then((res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.deep.equal(this.noteFixture);
+        });
+    });
+
+    it('should return a location header corresponding to the new item', function () {
+      let location;
+      return this.agent
+        .post('/api/notes')
+        .send(this.noteFixture)
+        .then((res) => {
+          expect(res).to.have.header('Location');
+          location = new URL(res.header.location);
+        })
+        .then(() => this.agent.get(location.pathname))
+        .then((res) => {
+          expect(res).to.have.status(200);
+          /**
+           * Because of the caching behavior of require, simDB will actually not be
+           * cleared between calls. The fix would be to load simDB earlier (in app.js)
+           * and pass that instance further down the call chain. Then we could use the
+           * .on('close') listener to clean up before our tests.
+           */
+          expect(res.body).to.deep.equal(Object.assign(this.noteFixture, { id: 1011 }));
+        });
+    });
+  });
+
+  context('with missing title field', function () {
+    it('should return an object with the correct message field', function () {
+      const noteFixture = { id: 1010, content: 'No, dogs are better than rabbits' };
+      return chai
+        .request(app)
+        .post('/api/notes')
+        .send(noteFixture)
+        .then((res) => {
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('Missing `title` in request body');
+        });
     });
   });
 });
